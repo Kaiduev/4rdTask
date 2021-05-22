40ba38084c152965f045d2a281507f1af6f2a1b6
Baktygul Kaiduev <baktygulkaiduev@Baktyguls-MacBook-Pro.local>
2021-05-22 23:28:05 +0600

    first commit

---
+++ UIapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIapp
+//
+//  Created by Baktygul Kaiduev on 20/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
